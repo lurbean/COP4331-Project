bda2effae58b8c09230cdf8e3e0ff5d246b2891b
atl1412 <atlstudent1412@gmail.com>
2018-09-10 18:45:53 -0400

    Added GUI

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,15 +4,45 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.PongGame;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    private JButton settingsButton;
+    private JTextArea welcomeToMortalPongbatTextArea;
+    private JPanel panel1;
+    private JButton playButton;
+    private JButton button2;
+
+    public DesktopLauncher() {
+
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
 
-        config.title = "Pong"; //Title of Window
-        config.width = 800;
-        config.height = 480;
-        new LwjglApplication(new PongGame(), config);
+                config.title = "Pong"; //Title of Window
+                config.width = 800;
+                config.height = 480;
+                new LwjglApplication(new PongGame(), config);
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "You are in the Settings");
+            }
+        });
+    }
+
+    public static void main (String[] arg) {
+        JFrame frame = new JFrame("DesktopLauncher");
+        frame.setContentPane(new DesktopLauncher().panel1);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
 
 	}
 }
