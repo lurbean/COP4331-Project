f5f3869aa78a85a99270b68e63069a0f51b76a22
Adam <adambush33@gmail.com>
2018-10-15 10:50:50 -0400

    added a settings window with test function and confirmed it worked, excepting the cooldown part

--- core/src/com/mygdx/game/SETTINGS.java
+++ core/src/com/mygdx/game/SETTINGS.java
@@ -11,8 +11,28 @@ public class SETTINGS {
     public int P2Damage = 100;
     public int P2HitSpeed = 100;
     public int P2Cooldown = 100;
+    public int gameLength = 60;
     public int AllBallSpeed = 100;
     public int AllHealthMod = 100;
     public int AllCooldown = 100;
-    public int gameLength = 60;
+
+
+    public void DisplaySettings()
+    {
+        System.out.println("The set of settings selected are below, in the order found in the SETTINGS class");
+        System.out.println(P1HealthMod);
+        System.out.println(P1PaddleSpeed);
+        System.out.println(P1Damage);
+        System.out.println(P1HitSpeed);
+        System.out.println(P1Cooldown);
+        System.out.println(P2HealthMod);
+        System.out.println(P2PaddleSpeed);
+        System.out.println(P2Damage);
+        System.out.println(P2HitSpeed);
+        System.out.println(P2Cooldown);
+        System.out.println(gameLength);
+        System.out.println(AllBallSpeed);
+        System.out.println(AllHealthMod);
+        System.out.println(AllCooldown);
+    }
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,16 +5,51 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.*;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.ImageObserver;
 
 public class DesktopLauncher {
-    private JButton settingsButton;
-    private JPanel panel1;
+    private JPanel MainMenu;
+    private JTabbedPane Tabbed;
     private JButton playButton;
-    private JButton button2;
+    private JPanel SettingsWindow;
+    private JSplitPane LeftSplit;
+    private JSplitPane RightSplit;
+    private JPanel LeftPanel;
+    private JPanel GameSettings;
+    private JComboBox GameLength;
+    private JComboBox GlobalHealthMultiplier;
+    private JComboBox GlobalBallSpeedMod;
+    private JComboBox GlobalCooldownMod;
+    private JPanel RightPanel;
+    private JPanel PlayerTwo;
+    private JComboBox P2HealthMultiplier;
+    private JComboBox P2PaddleSpeedMod;
+    private JComboBox P2AttackDamageMod;
+    private JComboBox P2HitSpeedMod;
+    private JComboBox P2CooldownMod;
+    private JPanel PlayerOne;
+    private JComboBox P1HealthMultiplier;
+    private JComboBox P1PaddleSpeedMod;
+    private JComboBox P1AttackDamageMod;
+    private JComboBox P1HitSpeedMod;
+    private JComboBox P1CooldownMod;
 
-    SETTINGS settings = new SETTINGS();
+    private int PlayerAttack[] = {50, 75, 100, 125, 150, 200, 300};
+    private int PlayerHealth[] = {0, 25, 50, 100, 150, 200, 300, 500};
+    private int PlayerPaddle[] = {50, 75, 100, 125, 150, 200};
+    private int PlayerBallHit[] = {50, 75, 100, 125, 150, 175, 200};
+    private int PlayerCooldown[] = {50, 75, 100, 125, 150, 0};
+
+    private int GameDuration[] = {0, 30, 45, 60, 75, 99};
+    private int GlobalHealth[] = {0, 50, 100, 200, 300, 500};
+    private int GlobalBall[] = {50, 100, 150, 200};
+    private int GlobalCooldown[] = {50, 75, 100, 125, 150, 0};
+    private int temp;
+
+    public SETTINGS settings = new SETTINGS();
 
     public DesktopLauncher() {
 
@@ -27,23 +62,130 @@ public class DesktopLauncher {
                 config.title = "Pong"; //Title of Window
                 config.width = 800;
                 config.height = 580;
+                settings.DisplaySettings();
                 new LwjglApplication(new PongGame(settings), config);
             }
         });
-        button2.addActionListener(new ActionListener() {
+
+        P1HealthMultiplier.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "You are in the Settings");
+                temp = P1HealthMultiplier.getSelectedIndex();
+                temp = PlayerHealth[temp];
+                settings.P1HealthMod = temp;
+            }
+        });
+        P1PaddleSpeedMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = P1PaddleSpeedMod.getSelectedIndex();
+                temp = PlayerPaddle[temp];
+                settings.P1PaddleSpeed = temp;
+            }
+        });
+        P1AttackDamageMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = P1AttackDamageMod.getSelectedIndex();
+                temp = PlayerAttack[temp];
+                settings.P1Damage = temp;
+            }
+        });
+        P1HitSpeedMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = P1HitSpeedMod.getSelectedIndex();
+                temp = PlayerBallHit[temp];
+                settings.P1HitSpeed = temp;
+            }
+        });
+        P1CooldownMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = P1CooldownMod.getSelectedIndex();
+                temp = PlayerCooldown[temp];
+                settings.P1Cooldown = temp;
+            }
+        });
+        GameLength.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = GameLength.getSelectedIndex();
+                temp = GameDuration[temp];
+                settings.gameLength = temp;
+            }
+        });
+        GlobalHealthMultiplier.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = GlobalHealthMultiplier.getSelectedIndex();
+                temp = GlobalHealth[temp];
+                settings.AllHealthMod = temp;
+            }
+        });
+        GlobalBallSpeedMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = GlobalBallSpeedMod.getSelectedIndex();
+                temp = GlobalBall[temp];
+                settings.AllBallSpeed = temp;
+            }
+        });
+        GlobalCooldownMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = GlobalCooldownMod.getSelectedIndex();
+                temp = GlobalCooldown[temp];
+                settings.AllCooldown = temp; //TODO: cooldowns need to be modifiable values and this needs to be applied to them. If settings.AllCooldown==0, Cooldown takes forever. Otherwise, Cooldown takes normalCooldown * settings.AllCooldown / 100. Same for P1Cooldown and P2Cooldown, for their respective sides
+            }
+        });
+        P2HealthMultiplier.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = P2HealthMultiplier.getSelectedIndex();
+                temp = PlayerHealth[temp];
+                settings.P2HealthMod = temp;
+            }
+        });
+        P2PaddleSpeedMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = P2PaddleSpeedMod.getSelectedIndex();
+                temp = PlayerPaddle[temp];
+                settings.P2PaddleSpeed = temp;
+            }
+        });
+        P2AttackDamageMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = P2AttackDamageMod.getSelectedIndex();
+                temp = PlayerAttack[temp];
+                settings.P2Damage = temp;
+            }
+        });
+        P2HitSpeedMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = P2HitSpeedMod.getSelectedIndex();
+                temp = PlayerBallHit[temp];
+                settings.P2HitSpeed = temp;
+            }
+        });
+        P2CooldownMod.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                temp = P2CooldownMod.getSelectedIndex();
+                temp = PlayerCooldown[temp];
+                settings.P2Cooldown = temp;
             }
         });
     }
 
     public static void main (String[] arg) {
         JFrame frame = new JFrame("DesktopLauncher");
-        frame.setContentPane(new DesktopLauncher().panel1);
+        frame.setContentPane(new DesktopLauncher().Tabbed);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-
 	}
 }
