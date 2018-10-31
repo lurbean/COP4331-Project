6b994ed5be71af19ca6014ec75334cf252194bd7
abush33 <adambush33@gmail.com>
2018-10-31 18:00:19 -0400

    added test library

---
+++ core/src/test/java/com/mygdx/game/BallTest.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BallTest {
+
+    @Test
+    void switchVelocity() {
+    }
+}
